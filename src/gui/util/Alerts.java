7d1400be4978cbfccc4b26120aa896c2d06d20da
Felipe Nanini Cavelagna <felipenanini@gmail.com>
2021-10-25 01:35:22 -0300

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,15 +1,32 @@
 package gui.util;
 
+import java.util.Optional;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.text.Text;
 
 public class Alerts {
 
 	public static void showAlert(String title, String header, String content, AlertType type) {
 		Alert alert = new Alert(type);
+		
+		Text text = new Text(content);
+		text.setWrappingWidth(350);
+		
 		alert.setTitle(title);
 		alert.setHeaderText(header);
-		alert.setContentText(content);
+		alert.getDialogPane().setContent(text);
+		//alert.setContentText(content);
 		alert.show();
+	}
+
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
 	}
 }
